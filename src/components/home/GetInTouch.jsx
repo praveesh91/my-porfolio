5e33917027c11752f21f58ea9319e5011c406490
praveesh91 <praveesh91@gmail.com>
2021-12-04 23:58:59 +0530

    resume link update contact form layout for sm screen

--- src/components/home/GetInTouch.jsx
+++ src/components/home/GetInTouch.jsx
@@ -61,14 +61,6 @@ const GetInTouch = ({ heading, message, email }) => {
                   placeholder="Your Phone Number *"
                 />
               </div>
-              <div class="form-group">
-                <input
-                  type="submit"
-                  name="btnSubmit"
-                  class="btnContact"
-                  value="Send"
-                />
-              </div>
             </div>
             <div class="col-md-6">
               <div class="form-group">
@@ -77,6 +69,19 @@ const GetInTouch = ({ heading, message, email }) => {
                   class="form-control"
                   placeholder="Your Message *"
                   style={{ width: "100%", height: "150px" }}></textarea>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <input
+                  type="submit"
+                  name="btnSubmit"
+                  class="btnContact"
+                  value="Send"
+                />
               </div>
             </div>
           </div>
